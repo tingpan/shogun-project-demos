7c742ff1e722d5f1a5e9389aa8906d4fb4d1c26e
Ting Pan <tingpan666@gmail.com>
2017-06-13 03:28:04 +0800

    ADD more scripts

--- run.py
+++ run.py
@@ -1,40 +1,39 @@
 # functions for represent each team with the average score and played-time percentage for each type
 import json
 
+PLAY_TYPES = ['Transition', 'Isolation', 'PRBallHandler', 'PRRollman', 'Postup', 'Spotup', 'Handoff', 'Cut',
+              'OffScreen', 'OffRebound', 'Misc']
 with open('result.json') as data_file:
     data = json.load(data_file)
 
-
 with open('data/dashboard/2015-16/2015-16_play_types_dashboard.json') as data_file:
     play_types = json.load(data_file)
-
 
 with open('data/dashboard/2015-16/2015-16_general_dashboard.json') as data_file:
     general_dashboard = json.load(data_file)
 
 i = 0
-players = []
-for player in play_types['PlayTypesPerGame']['Transition']:
-    name = player['PLAYER_FIRST_NAME'].replace(' ', '-') + ' ' + player['PLAYER_LAST_NAME'].replace(' ', '-')
-    _name = player['PLAYER_FIRST_NAME'] + ' ' + player['PLAYER_LAST_NAME']
-    _other_name = player['PLAYER_FIRST_NAME'].replace(' ', '') + ' ' + player['PLAYER_LAST_NAME'].replace(' ', '')
-    category = None
-    if data.has_key(name):
-        category = data[name]
-    elif data.has_key(_name):
-        category = data[_name]
-    elif data.has_key(_other_name):
-        category = data[_other_name]
-    else:
-        i+=1
+players = {}
+teams = {}
+team_mins = {}
+print(data['Evan Fournier'])
+for player in general_dashboard['GeneralStatsPerGame']['Base']:
+    players[player['PLAYER_ID']] = player
+    players[player['PLAYER_ID']]['Cluster'] = data[player['PLAYER_NAME']]
+    players[player['PLAYER_ID']]['Minutes'] = player['MIN'] * player['GP']
+    if not team_mins.has_key(player['TEAM_ABBREVIATION']): team_mins[player['TEAM_ID']] = 0
+    team_mins[player['TEAM_ABBREVIATION']] += player['MIN'] * player['GP']
+    if not team_mins.has_key(player['TEAM_ABBREVIATION']): teams[player['TEAM_ID']] = []
+    teams[player['TEAM_ABBREVIATION']].append(players[player['PLAYER_ID']])
 
-    if category:
-        player_info = player
-        player_info['CATEGORY'] = category
-        players.append(player_info)
+play_type_data = {}
+for type in PLAY_TYPES:
+    player_data = play_types['PlayTypesPerGame'][type]
+    play_type_data[type] = {}
+    for player in player_data:
+        play_type_data[type][player['PLAYER_ID']] = player
 
-
-print(players[0]['PLAYER_ID'])
-# print(len(data))
-# print((play_types['PlayTypesPerGame']['Transition'][0]))
-print(general_dashboard['GeneralStatsPerGame']['Base'][0].keys())
+team_scores = {}
+for team_abbr, team in teams.iteritems():
+    for player in team:
+        player['Minutes'] / team_mins[team_abbr] * 
