3fc07806fff406329109f896b28b5300a4af63cb
Ting Pan <tingpan666@gmail.com>
2017-06-14 03:53:50 +0800

    ADD more data parser

--- run.py
+++ run.py
@@ -36,11 +36,10 @@ def prepare_data():
 
     team_scores = {}
     for team_abbr, team in teams.iteritems():
-        team_score = [{'percent': 0, 'total_score': 0, 'player_count': 0} for i in range(0,13)]
+        team_score = [{'percent': 0, 'total_score': 0} for i in range(0,13)]
         for player in team:
             percent = player['Minutes'] / team_mins[team_abbr]
             team_score[player['Cluster']]['percent'] += percent
-            team_score[player['Cluster']]['player_count'] += 1
             score = 0
             for type in PLAY_TYPES:
                 if play_type_data[type].has_key(player['PLAYER_ID']):
@@ -50,6 +49,43 @@ def prepare_data():
         team_scores[team_abbr] = team_score
     return team_scores
 
-data = prepare_data()['NYK']
-for category in data:
-    print(category)
+def prepare_game_data(team_scores):
+    with open('data/gamelog.json') as data_file:
+        game_results = json.load(data_file)
+    game_scores = {}
+    for game in game_results:
+        if '@' in game['MATCHUP']:
+            game_scores[game['GAME_ID']] = []
+            team_abbrs = game['MATCHUP'].split(' @ ')
+            for team_abbr in team_abbrs:
+                game_scores[game['GAME_ID']] += team_scores[team_abbr]
+            game_scores[game['GAME_ID']] += game['WL']
+    return game_scores
+
+def save_to_files():
+    team_scores = prepare_data()
+    game_data = prepare_game_data(team_scores)
+    test_data = ''
+    test_label = ''
+    train_data = ''
+    train_label = ''
+    parsed_data = []
+    for game_id, data in game_data.iteritems():
+        parsed_data.append([' '.join([' '.join([str(values['percent']), str(values['total_score'])]) for values in data[:-1]]), data[-1]])
+    for data in parsed_data[:-70]:
+        train_data += data[0] + '\n'
+        train_label += data[1] + '\n'
+    for data in parsed_data[:]:
+        test_data += data[0] + '\n'
+        test_label += data[1] + '\n'
+    with open('test.data', 'w') as file:
+        file.write(test_data)
+    with open('train.data', 'w') as file:
+        file.write(train_data)
+    with open('test.label', 'w') as file:
+        file.write(test_label)
+    with open('train.label', 'w') as file:
+        file.write(train_label)
+
+
+save_to_files()
